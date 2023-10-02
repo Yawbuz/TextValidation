1bd488b54cc1208eef366ab4468365d95e67a37d
spoons <ev4n.mus1c@gmail.com>
2023-10-02 16:16:36 -0600

    This will also be the social media app :)

--- TextValidaton/ViewController.swift
+++ TextValidaton/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var emailErrorLabel: UILabel!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    
 
+    
     override func viewDidLoad() {
+        passwordErrorLabel.isHidden = true
+        emailErrorLabel.isHidden = true
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
+    
 
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        checkPassword()
+    }
+    
+    func checkEmail() {
+        
+    }
+    func checkPassword() {
+        if let checkIt = passwordField.text {
 
+            if checkIt.count < 8 {
+                passwordErrorLabel.text = "Minimum character amount:8"
+                passwordErrorLabel.isHidden = false
+                return
+            }
+            if !checkIt.contains(where: { "@,.!@#$%^&*<>?+=-".contains($0) }) {// $0 represents each character in the string. contains is cool like that
+                passwordErrorLabel.text = "Special character required (@,.!@#$%^&*<>?+=-)"
+                print(checkIt)
+                passwordErrorLabel.isHidden = false
+                return
+            }
+            
+            performSegue(withIdentifier: "toLanding", sender: checkIt)
+            
+        }
+        
+    }
 }
-
---
+++ userAccount].swift
@@ -0,0 +1,14 @@
+//
+//  userAccount].swift
+//  TextValidaton
+//
+//  Created by shark boy on 10/2/23.
+//
+
+import Foundation
+
+struct Account{
+    var email = String()
+    var password = String()
+}
+
